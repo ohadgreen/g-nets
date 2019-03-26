febd473f525eda8df69dd593c09ab3c50cf1bd59
ohadgreen <ohad.green@gmail.com>
2019-03-26 21:29:09 +0200

    server keys

---
+++ server/config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    cryptoCompareApiKey: process.env.CRYPTO_COMPARE_API_KEY,
+    year: process.env.YEAR,
+    seasonType: process.env.SEASON_TYPE,
+  };
---
+++ server/resources/images/avatarImageList.js
@@ -0,0 +1,18 @@
+const allAvatars = [
+    "cougar",
+    "dribble",
+    "flying",
+    "green",
+    "kobe",
+    "lebron",
+    "mickey",
+    "mjordan",
+    "no5",
+    "no10",
+    "no12",
+    "no99",
+    "purple",
+    "shark",
+    "swivel"
+  ]
+module.exports = allAvatars;
