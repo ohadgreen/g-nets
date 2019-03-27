3c247ad9409e8b2a11343e115fa7f9e89251f72f
ohadgreen <ohad.green@gmail.com>
2019-03-27 18:46:50 +0200

    resolve path build dir

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const router = express.Router();
 require('./model/Game');
 require('./model/Team');
@@ -24,8 +25,16 @@ require('./routes/app/FetchGames')(app);
 require('./routes/app/FetchScores')(app);
 
 if (process.env.NODE_ENV === "production") {
+    console.log('*** prod build files');
+    const buildPath = path.resolve(__dirname, "../client/build");
+    fs.readdir(buildPath, (err, files) => {
+        files.forEach(file => {
+          files.push(file);
+          console.log(file);
+        });
+      });
     // tell Express to server production assets like main.js
-    app.use(express.static(__dirname + "../client/build"));
+    app.use(express.static(buildPath));
   
     // Express will server the index.html file if it doesn't recognize the route
     const path = require("path");
