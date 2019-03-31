f96f71ba791a5b01edd4aaafe8c0c116b6684495
ohadgreen <ohad.green@gmail.com>
2019-03-31 13:40:21 +0300

    CORS

--- server/index.js
+++ server/index.js
@@ -20,6 +20,14 @@ app.use(bodyParser.urlencoded({
     extended: false,
 }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
+});
+
 require('./routes/auth/UserAuth')(app);
 require('./routes/app/FetchGames')(app);
 require('./routes/app/FetchScores')(app);
