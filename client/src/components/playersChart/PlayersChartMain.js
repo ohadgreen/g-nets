b4d5101ba032a9ce8e8748d7a79fafa330e98a26
ohadgreen <ohad.green@gmail.com>
2019-04-01 18:31:16 +0300

    table design done

--- client/src/components/playersChart/PlayersChartMain.js
+++ client/src/components/playersChart/PlayersChartMain.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as playersChartActions from "../../store/playerScores/actions";
-import { AllScores } from "../gameBet/AllScores";
+import { RenderAvatar } from "../common/RenderAvatar";
+import { Table } from "semantic-ui-react";
 import "./PlayersChart.css";
 
 class PlayersChartMain extends Component {  
@@ -9,13 +10,45 @@ class PlayersChartMain extends Component {
     this.props.dispatch(playersChartActions.allPlayerScores());
   }
 
+  renderUserScore = (user, i) => {
+    return (
+      <tr key={i}>
+        <td>{i+1}</td>
+        <td><RenderAvatar user={user} /></td>
+        <td>{user.bets.totalBets}</td>
+        <td>{user.bets.totalScore}</td>
+        <td>{user.bets.avgScore.toFixed(2)}</td>
+      </tr>
+    );
+  }
+
   render() {
     if (this.props.allPlayerScores.length === 0) {
       return <div>Fetching info...</div>;
     } else {      
       return (
-        <div className="total-chart-container">
-          <div className="total-chart-content"><AllScores allScores={this.props.allPlayerScores}/></div>
+        <div className="players-chart-container">
+        <div className="players-chart-header">Overall Standings</div>
+          <div className="players-chart-content">
+          <Table celled striped compact size="small">
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>Rank</Table.HeaderCell>
+              <Table.HeaderCell>Player</Table.HeaderCell>
+              <Table.HeaderCell>Total Bets</Table.HeaderCell>
+              <Table.HeaderCell>Total Score</Table.HeaderCell>
+              <Table.HeaderCell>Average Score</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {this.props.allPlayerScores.map((bet, i) => {
+              if (i < 10) {
+                return this.renderUserScore(bet, i);
+              } else return "";
+            })}
+          </Table.Body>
+        </Table>
+        </div>
         </div>
       );
     }
